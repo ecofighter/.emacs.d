d0f668ee2e7592a20d294bf164a1f3cae4e6d790
Shota Arakaki <syotaa1@gmail.com>
2017-12-10 01:51:37 +0900

    delete unused plugin config

--- inits/10-elscreen.el
+++
@@ -1,6 +0,0 @@
-;; (install-when-compile 'elscreen)
-
-;; (setup-include "elscreen"
-;;   (elscreen-start))
-
-;; (provide-file)
--- inits/10-powerline.el
+++
@@ -1,9 +0,0 @@
-(install-when-compile 'powerline)
-(install-when-compile 'powerline-evil)
-
-(setup-include "powerline")
-
-(setup-include "powerline-evil"
-  (powerline-evil-vim-color-theme))
-
-(provide-file)
