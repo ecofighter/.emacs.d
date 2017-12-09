9270e7a81efc04b549238522067bd86885bda80b
Shota Arakaki <syotaa1@gmail.com>
2017-12-10 01:52:00 +0900

    config for c

--- inits/30-c++.el
+++ inits/30-c++.el
@@ -10,72 +10,42 @@
 (add-hook 'c++-mode-hook #'yas-minor-mode-on)
 (add-hook 'c++-mode-hook #'flycheck-mode-on-safe)
 
-
-;; (use-package cmake-ide
-;; 	:defer t
-;; 	:init (add-hook 'c++-mode-hook #'(lambda ()
-;; 																		 (eval-after-load 'rtags
-;; 																			 (cmake-ide-setup))))
-;; 	:config
-;; 	(setq cmake-ide-build-dir "build")
-;; 	(evil-leader/set-key-for-mode 'c++-mode
-;; 		"m c" 'cmake-ide-compile))
+(defmacro do-c-and-c++-mode (&rest body)
+  "Anaphoric macro provide mode to eval BODY for C and C++ mode."
+  `(dolist (mode '(c++-mode c-mode))
+     ,@body))
 
 (setup-lazy '(cmake-ide-setup) "cmake-ide")
 (setup-expecting "cmake-ide"
-  (add-hook 'c++-mode-hook #'(lambda ()
-                               (setup "rtags"
-                                 (setq cmake-ide-build-dir "build")
-                                 (cmake-ide-setup)))))
+  (dolist (hook '(c++-mode-hook c-mode-hook))
+    (add-hook hook #'(lambda ()
+                       (setup "rtags"
+                         (setq-default cmake-ide-build-dir "build")
+                         (cmake-ide-setup))))))
 
 (setup-after "cmake-ide"
-  (evil-leader/set-key-for-mode 'c++-mode
-    "m c" 'cmake-ide-compile))
-
-;; (use-package rtags
-;; 	:defer t
-;; 	:init
-;; 	(evil-leader/set-key-for-mode 'c++-mode
-;; 		"m r s" 'rtags-find-symbol-at-point)
-;; 	(evil-leader/set-key-for-mode 'c++-mode
-;; 		"m r r" 'rtags-find-references-at-point)
-;; 	:config
-;; 	(require 'ivy-rtags)
-;; 	(setq rtags-display-result-backend 'ivy)
-;; 	(setq rtags-autostart-diagnostics t))
+  (setup-after "evil-leader"
+    (do-c-and-c++-mode
+     (evil-leader/set-key-for-mode mode
+       "m c" 'cmake-ide-compile))))
 
 (setup-after "rtags"
   (setup "ivy-rtags")
   (setq rtags-display-result-backend 'ivy)
   (setq rtags-autostart-diagnostics t)
-  (evil-leader/set-key-for-mode 'c++-mode
-    "m r s" 'rtags-find-symbol-at-point)
-  (evil-leader/set-key-for-mode 'c++-mode
-    "m r r" 'rtags-find-references-at-point))
-
-;; (use-package irony
-;; 	:defer t
-;; 	:init (add-hook 'c++-mode-hook #'irony-mode)
-;; 	:config
-;; 	(eval-after-load 'cmake-ide (irony-cdb-autosetup-compile-options)))
+  (do-c-and-c++-mode
+   (evil-leader/set-key-for-mode mode
+     "m r s" 'rtags-find-symbol-at-point)
+   (evil-leader/set-key-for-mode mode
+     "m r r" 'rtags-find-references-at-point)))
 
 (setup-lazy '(irony-mode) "irony"
   (setup-after "cmake-ide"
     (irony-cdb-autosetup-compile-options)))
-(setup-expecting "irony"
-  (add-hook 'c++-mode-hook #'irony-mode))
 
-;; (use-package company-irony
-;; 	:defer t
-;; 	:init (progn
-;; 					(add-hook 'c++-mode-hook
-;; 										#'(lambda ()
-;; 												(eval-after-load 'company
-;; 													'(add-to-list
-;; 														'company-backends 'company-irony))))
-;; 					(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands))
-;; 	:config
-;; 	(setq company-backends (delete 'company-semantic company-backends)))
+(setup-expecting "irony"
+  (dolist (hook '(c++-mode-hook c-mode-hook))
+    (add-hook hook #'irony-mode)))
 
 (setup-after "irony"
   (setup-after "company"
@@ -87,13 +57,6 @@
                           (delete 'company-semantic company-backends))
                     (company-irony-setup-begin-commands))))))
 
-;; (use-package company-irony-c-headers
-;; 	:defer t
-;; 	:init (add-hook 'c++-mode-hook
-;; 									#'(lambda ()
-;; 											(eval-after-load 'company
-;; 												'(add-to-list 'company-backends 'company-irony-c-headers)))))
-
 (setup-after "irony"
   (setup-after "company"
     (setup "company-irony-c-headers"
@@ -101,28 +64,13 @@
                 #'(lambda ()
                     (add-to-list 'company-backends 'company-irony-c-headers))))))
 
-;; (use-package flycheck-irony
-;;   :defer t
-;;   :init (add-hook 'c++-mode-hook
-;;                   #'(lambda ()
-;;                       (eval-after-load 'flycheck
-;;                         '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))))
-
 (setup-lazy '(flycheck-irony-setup) "flycheck-irony")
 (setup-after "flycheck"
   (add-hook 'irony-mode-hook #'flycheck-irony-setup))
 
-;; (use-package clang-format
-;;   :defer t
-;;   :commands (clang-format-buffer)
-;;   :init
-;;   (evil-leader/set-key-for-mode 'c++-mode
-;;     "m =" 'clang-format-buffer)
-;;   :config
-;;   (setq clang-format-style "file"))
-
 (setup-lazy '(clang-format-buffer) "clang-format")
-(evil-leader/set-key-for-mode 'c++-mode
-  "m =" 'clang-format-buffer)
+(do-c-and-c++-mode
+ (evil-leader/set-key-for-mode mode
+   "m =" 'clang-format-buffer))
 
 (provide-file)
