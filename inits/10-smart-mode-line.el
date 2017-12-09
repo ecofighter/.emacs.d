a1efb522a9f38e235aea07bd584d22311a05b29c
Shota Arakaki <syotaa1@gmail.com>
2017-12-10 01:54:11 +0900

    add smart-mode-line

--- inits/10-smart-mode-line.el
+++ inits/10-smart-mode-line.el
@@ -2,6 +2,8 @@
 
 (setup-include "smart-mode-line"
   (column-number-mode 1)
+  (setq sml/name-width '(5 . 25))
+  (setq sml/mode-width 'full)
   (setq sml/hidden-modes '(" Server" " WK" " ivy" " fd" " Undo-Tree" " WS" " SP" " company"))
   (setq evil-insert-state-tag (propertize " <I> " 'face '((:background "#076678"))))
   (setq evil-visual-state-tag (propertize " <V> " 'face '((:background "#fe8019"))))
