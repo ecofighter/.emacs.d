d500a09d4d342ecb05959228f070c1f0316ae51c
Shota Arakaki <syotaa1@gmail.com>
2017-12-10 01:55:49 +0900

    use context-skk-mode

--- inits/20-ddskk.el
+++ inits/20-ddskk.el
@@ -1,5 +1,8 @@
 (install-when-compile 'ddskk)
 
+(setup-expecting "skk"
+  (setq default-input-method "japanese-skk"))
+
 (setup-lazy '(skk-mode) "skk"
   (setq skk-kutouten-type 'en)
   (setq skk-use-azik t)
@@ -8,11 +11,13 @@
   (setq skk-server-portnum 1178))
 
 (setup-after "skk"
-  (setup "context-skk"
-    (add-hook 'skk-load-hook #'(lambda ()
-                                 (context-skk-mode 1)))))
+  (setup "context-skk")
+  (add-hook 'skk-load-hook #'(lambda ()
+                               (context-skk-mode 1))))
 
-(setup-expecting "skk"
-  (setq default-input-method "japanese-skk"))
+(setup-lazy '(context-skk-mode
+              context-skk-context-check
+              context-skk-insert
+              context-skk-customize) "context-skk")
 
 (provide-file)
