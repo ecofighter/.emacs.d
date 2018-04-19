59a393e9a1db767c384f5fbe4625bc12527f155e
Shota Arakaki <syotaa1@gmail.com>
2018-04-19 19:51:12 +0900

    Change skk startup key

--- inits/20-ddskk.el
+++ inits/20-ddskk.el
@@ -1,23 +1,27 @@
 (install-when-compile 'ddskk)
 
 (setup-expecting "skk"
+  (setup-include "skk-autoloads")
+  (global-set-key (kbd "C-x j") 'skk-mode)
+  (global-set-key (kbd "C-x J") 'skk-auto-fill-mode)
   (setq default-input-method "japanese-skk"))
 
-(setup-lazy '(skk-mode) "skk"
+(setup-after "skk"
   (setq-default skk-kutouten-type '("．" . "，"))
   (setq-default skk-use-azik t)
+  (setup "skk-study")
   (setq-default skk-user-directory "~/.skk-jisyo")
   (setq-default skk-server-host "127.0.0.1")
   (setq-default skk-server-portnum 1178))
 
-(setup-after "skk"
-  (setup "context-skk")
-  (add-hook 'skk-load-hook #'(lambda ()
-                               (context-skk-mode 1))))
+;; (setup-after "skk"
+;;   (setup "context-skk")
+;;   (add-hook 'skk-load-hook #'(lambda ()
+;;                                (context-skk-mode 1))))
 
-(setup-lazy '(context-skk-mode
-              context-skk-context-check
-              context-skk-insert
-              context-skk-customize) "context-skk")
+;; (setup-lazy '(context-skk-mode
+;;               context-skk-context-check
+;;               context-skk-insert
+;;               context-skk-customize) "context-skk")
 
 (provide-file)
