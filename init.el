a038f05d39bd7dc43cba8b7b1d81bc05c1cd4b5f
Shota Arakaki <syotaa1@gmail.com>
2019-02-03 01:07:58 +0900

    fix

--- init.el
+++ init.el
@@ -43,7 +43,7 @@
 (require '30-emacslisp)
 ;; (require '30-c++)
 ;; (require '30-bison)
-;; (setup-include "30-cmake")
+;; (require '30-cmake)
 (require '30-latex)
 (require '30-org)
 (require '30-common-lisp)
@@ -59,46 +59,4 @@
 
 (install-when-compile 'package-utils)
 (provide 'init)
-;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
- '(avy-migemo-function-names
-   (quote
-    (swiper--add-overlays-migemo
-     (swiper--re-builder :around swiper--re-builder-migemo-around)
-     (ivy--regex :around ivy--regex-migemo-around)
-     (ivy--regex-ignore-order :around ivy--regex-ignore-order-migemo-around)
-     (ivy--regex-plus :around ivy--regex-plus-migemo-around)
-     ivy--highlight-default-migemo ivy-occur-revert-buffer-migemo ivy-occur-press-migemo avy-migemo-goto-char avy-migemo-goto-char-2 avy-migemo-goto-char-in-line avy-migemo-goto-char-timer avy-migemo-goto-subword-1 avy-migemo-goto-word-1 avy-migemo-isearch avy-migemo-org-goto-heading-timer avy-migemo--overlay-at avy-migemo--overlay-at-full)))
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "7f89ec3c988c398b88f7304a75ed225eaac64efa8df3638c815acc563dfd3b55" "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c" "595617a3c537447aa7e76ce05c8d43146a995296ea083211225e7efc069c598f" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(package-selected-packages
-   (quote
-    (org-plus-contrib proof-general yasnippet-snippets ess company-reftex evil-terminal-cursor-changer latex-math-preview evil-org multi-term evil-collection org-pomodoro cargo rust-mode yaml-mode omnisharp highlight-indent-guides fsharp-mode eglot sml-mode company-coq cmake-mode lsp-ui winner-mode rg edit-indirect ripgrep company-lean lean-mode migemo which-key utop use-package tuareg spaceline smart-mode-line slime-company shackle rainbow-delimiters powerline-evil package-utils ocp-indent nlinum markdown-mode magit lsp-ocaml lsp-haskell key-chord ivy-rtags hl-todo gruvbox-theme flycheck-ocaml flycheck-irony exec-path-from-shell evil-tabs evil-smartparens evil-leader evil-escape ddskk counsel company-rtags company-quickhelp company-lsp company-irony-c-headers company-irony company-auctex cmake-ide clang-format bison-mode auctex-latexmk)))
- '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#32302f")))
- '(winner-mode t))
-
-(provide 'init)
 ;;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(agda2-highlight-datatype-face ((t (:foreground "#E6DB74"))))
- '(agda2-highlight-field-face ((t (:foreground "#A6E22E"))))
- '(agda2-highlight-function-face ((t (:foreground "#A6E22E"))))
- '(agda2-highlight-inductive-constructor-face ((t (:foreground "#F92672"))))
- '(agda2-highlight-keyword-face ((t (:foreground "#66D9EF"))))
- '(agda2-highlight-module-face ((t (:foreground "#AE81FF"))))
- '(agda2-highlight-number-face ((t (:foreground "#AE81FF"))))
- '(agda2-highlight-postulate-face ((t (:foreground "#E6DB74"))))
- '(agda2-highlight-primitive-face ((t (:foreground "#CE4045"))))
- '(agda2-highlight-primitive-type-face ((t (:foreground "#E6DB74"))))
- '(font-lock-comment-face ((t (:foreground "#75715E")))))
