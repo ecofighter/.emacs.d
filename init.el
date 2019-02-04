6c5f84ac3462ef722b0225434086529d3b3e90fb
Shota Arakaki <syotaa1@gmail.com>
2019-02-04 16:52:59 +0900

    c++

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;;; init.el --- my init.el           -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Code:
+(setq custom-file (locate-user-emacs-file "custom.el"))
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq
@@ -41,7 +42,6 @@
 (require '20-highlight-indent-guides)
 (require '20-magit)
 (require '30-emacslisp)
-;; (require '30-c++)
 ;; (require '30-bison)
 ;; (require '30-cmake)
 (require '30-latex)
@@ -54,33 +54,10 @@
 (require '30-coq)
 (require '30-lean)
 (require '31-lsp)
+(require '32-c++)
 (require '32-rust)
 (require '32-haskell)
 
 (install-when-compile 'package-utils)
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet-snippets yaml-mode which-key utop tuareg sml-mode smart-mode-line slime-company shut-up shackle rustic rg rainbow-delimiters proof-general package-utils org-pomodoro org-plus-contrib org-evil omnisharp ocp-indent nlinum magit lsp-ui lsp-haskell latex-math-preview ivy-rtags hl-todo highlight-indent-guides gruvbox-theme ghub fsharp-mode flycheck-ocaml flycheck-irony exec-path-from-shell evil-terminal-cursor-changer evil-smartparens evil-org evil-leader evil-escape evil-collection ess eglot ddskk cuda-mode counsel-projectile company-reftex company-lsp company-lean company-irony-c-headers company-irony company-coq company-auctex cmake-mode cmake-ide clang-format cargo bison-mode avy-migemo auctex-latexmk))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(agda2-highlight-datatype-face ((t (:foreground "#E6DB74"))))
- '(agda2-highlight-field-face ((t (:foreground "#A6E22E"))))
- '(agda2-highlight-function-face ((t (:foreground "#A6E22E"))))
- '(agda2-highlight-inductive-constructor-face ((t (:foreground "#F92672"))))
- '(agda2-highlight-keyword-face ((t (:foreground "#66D9EF"))))
- '(agda2-highlight-module-face ((t (:foreground "#AE81FF"))))
- '(agda2-highlight-number-face ((t (:foreground "#AE81FF"))))
- '(agda2-highlight-postulate-face ((t (:foreground "#E6DB74"))))
- '(agda2-highlight-primitive-face ((t (:foreground "#CE4045"))))
- '(agda2-highlight-primitive-type-face ((t (:foreground "#E6DB74"))))
- '(font-lock-comment-face ((t (:foreground "#75715E")))))
---
+++ inits/32-c++.el
@@ -0,0 +1,30 @@
+;;; 30-c++.el -- settings to write C++; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+(eval-when-compile (require 'mymacros))
+(install-when-compile 'cmake-ide)
+(install-when-compile 'ccls)
+
+(dolist (hook '(c++-mode-hook c-mode-hook))
+  (add-hook hook #'(lambda ()
+                     (setq-default cmake-ide-build-dir "build")
+                     (cmake-ide-setup)
+                     (require 'ccls)
+                     (lsp))))
+
+(eval-after-load "yasnippet"
+  (dolist (hook '(c++-mode-hook c-mode-hook))
+    (add-hook hook #'yas-minor-mode-on)))
+
+(eval-after-load "flycheck"
+  (dolist (hook '(c++-mode-hook c-mode-hook))
+    (add-hook hook #'flycheck-mode-on-safe)))
+
+(eval-after-load "cmake-ide"
+  (eval-after-load "evil-leader"
+    (dolist (mode '(c-mode c++-mode))
+      (evil-leader/set-key-for-mode mode
+        "m c" 'cmake-ide-compile))))
+
+(provide '32-c++)
+;;; 32-c++.el ends here
