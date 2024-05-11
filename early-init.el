ec5a6c32c1e970446590c05eddcae3bfe40f3834
Shota Arakaki <syotaa1@gmail.com>
2024-05-11 18:41:36 +0900

    default maxmized

--- early-init.el
+++ early-init.el
@@ -29,8 +29,7 @@
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . 0) default-frame-alist)
 (push '(undecorated . t) default-frame-alist)
-(push '(drag-internal-border . 1) default-frame-alist)
-(push '(internal-border-width . 5) default-frame-alist)
+(push '(fullscreen . maximized) default-frame-alist)
 
 (require 'package)
 (setq package-enable-at-startup nil)
