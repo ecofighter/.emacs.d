274ef82ef4d00110bfd088cd646290615dbb9d77
Shota Arakaki <syotaa1@gmail.com>
2026-01-05 19:03:10 +0900

    fix

--- init.el
+++ init.el
@@ -124,21 +124,22 @@
           (fcitx-aggressive-setup)
         (add-hook 'server-after-make-frame-hook #'fcitx-aggressive-setup)))
     ;; credit: yorickvP on Github
-    (defvar wl-copy-process nil)
-    (defun wl-copy (text)
-      (setq wl-copy-process (make-process :name "wl-copy"
-                                          :buffer nil
-                                          :command '("wl-copy" "-f" "-n")
-                                          :connection-type 'pipe
-                                          :noquery t))
-      (process-send-string wl-copy-process text)
-      (process-send-eof wl-copy-process))
-    (defun wl-paste ()
-      (if (and wl-copy-process (process-live-p wl-copy-process))
-          nil ; should return nil if we're the current paste owner
-        (shell-command-to-string "wl-paste -n | tr -d \r")))
-    (setq interprogram-cut-function 'wl-copy)
-    (setq interprogram-paste-function 'wl-paste))
+    (when (executable-find "wl-copy")
+      (defvar wl-copy-process nil)
+      (defun wl-copy (text)
+        (setq wl-copy-process (make-process :name "wl-copy"
+                                            :buffer nil
+                                            :command '("wl-copy" "-f" "-n")
+                                            :connection-type 'pipe
+                                            :noquery t))
+        (process-send-string wl-copy-process text)
+        (process-send-eof wl-copy-process))
+      (defun wl-paste ()
+        (if (and wl-copy-process (process-live-p wl-copy-process))
+            nil ; should return nil if we're the current paste owner
+          (shell-command-to-string "wl-paste -n | tr -d \r")))
+      (setq interprogram-cut-function 'wl-copy)
+      (setq interprogram-paste-function 'wl-paste)))
    ((eq system-type 'darwin)
     (custom-set-variables
      '(ns-command-modifier 'meta)
