d2c05a311e43c6904e94cad31e1b07517fc4d967
Shota Arakaki <syotaa1@gmail.com>
2026-08-27 16:16:32 +0900

    change theme

--- init.el
+++ init.el
@@ -371,7 +371,7 @@
   (modus-themes-italic-constructs t)
   :init
   (add-to-list 'custom-theme-load-path (locate-user-emacs-file "theme/"))
-  (modus-themes-load-theme 'kanagawa-wave))
+  (modus-themes-load-theme 'modus-operandi-tinted))
 (use-package fontaine
   :ensure t
   :autoload
